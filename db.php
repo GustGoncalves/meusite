853351fcc22e662b2cd61f2cd57657cde1a6f9a1
Gust <gustgoncalves7@gmail.com>
2025-03-30 02:49:46 -0300

    Commit pelo VS Code

--- db.php
+++ db.php
@@ -5,10 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "meusite_db";
 
-// Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
